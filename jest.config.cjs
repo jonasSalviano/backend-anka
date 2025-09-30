11509175824b27be857239c0aa6038c90bc22b2b
jonas <jonasalmeida49@gmail.com>
2025-09-30 17:02:59 -0300

    alterando o arquivo de configuraçãdo do teste unitario

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,15 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json', useESM: true }],
-  },
+  moduleFileExtensions: ['ts','tsx','js','json'],
+  transform: { '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json', useESM: true }] },
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
   collectCoverage: true,
-  coverageReporters: ['lcov', 'text-summary'],
+  coverageReporters: ['lcov','text-summary'],
   reporters: [
     'default',
-    ['jest-junit', { outputDirectory: 'coverage', outputName: 'test-report.xml' }],
+    ['jest-junit', { outputDirectory: 'coverage', outputName: 'junit.xml' }]
   ],
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
 };
